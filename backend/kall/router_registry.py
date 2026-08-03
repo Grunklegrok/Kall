e82fbf1d04ee4f58082afdf3514de9936bd290dc
GrunkleGrok <jdshattuck@gmail.com>
2026-08-02 19:35:15 -0700

    Register growth and resume intelligence APIs

--- backend/kall/router_registry.py
+++ backend/kall/router_registry.py
@@ -7,10 +7,12 @@ from kall.api_billing import router as billing_router
 from kall.api_brief import router as brief_router
 from kall.api_career_profiles import router as career_profiles_router
 from kall.api_documents import router as documents_router
+from kall.api_growth import router as growth_router
 from kall.api_intelligence import router as intelligence_router
 from kall.api_match_intelligence import router as match_intelligence_router
 from kall.api_opportunities import router as opportunities_router
 from kall.api_ops import router as operations_router
+from kall.api_resume_intelligence import router as resume_intelligence_router
 from kall.api_submissions import router as submissions_router
 from kall.api_tailoring import router as tailoring_router
 from kall.api_testimonials import router as testimonials_router
@@ -26,6 +28,8 @@ API_ROUTERS = (
     profile_router,
     intelligence_router,
     match_intelligence_router,
+    resume_intelligence_router,
+    growth_router,
     tailoring_router,
     documents_router,
     opportunities_router,
